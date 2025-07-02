84b62a03c8f8b85a7e40a5934fdbd7c5e95a9af6
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-07-02 16:12:55 +0200

    add: Banner component on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
-"use client";
 
-import Button from "@/app/components/Button";
+import { Amatic_SC } from 'next/font/google'
+import Banner from "./components/Banner";
+
+const amatic = Amatic_SC({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-amatic',
+});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-      <Button label="Zizi" onClick={() => console.log("c'est moi qui parle")} />
-      <Button label="Roamin" onClick={() => console.log("c'est moi qui parle")} />
-
-      
+    <div className="relative aspect-[16/9]">
+       <Banner
+        src="/assets/background.jpg"
+        alt="image bannière"
+        title="Donnons-leur autant qu'ils nous apportent"
+        paragraph="Chaque jour, des milliers d'animaux attendent une famille aimante. Trouvez votre compagnon idéal parmi nos animaux disponibles à l'adoption."
+        // containerClassName="w-full h-[400px]"
+        // imageClassName="object-cover brightness-70"
+        // titleClassName="font-amatic font-extrabold text-[2.3rem] text-center text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        // paragraphClassName="font-roboto absolute top-[250px] w-full text-center text-white"
+      />
     </div>
   );
 }
