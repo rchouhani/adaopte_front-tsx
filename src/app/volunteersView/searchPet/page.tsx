ea7759d01b329c52130ac07d2b9343dc5c5d16df
rchouhani <rchouhani@me.com>
2025-07-04 22:07:31 +0200

    add : fetch to the back

--- src/app/volunteersView/searchPet/page.tsx
+++ src/app/volunteersView/searchPet/page.tsx
@@ -1,5 +1,49 @@
 'use client'
 
-export default function SearchPet () {
+import { useEffect, useState } from "react";
 
+export default function SearchPet () {
+    interface Pet {
+        id: number,
+        pet_name: string,
+        status_id: string,
+        birthyear: Date,
+        breed: string,
+        pet_type: string,
+        city: string,
+        zipcode: string,
+        pet_description: string,
+        image_url: string,
+        created_at: Date,
+        updated_at: Date
+    }
+
+
+
+    const [pets, setPets] = useState<Pet[]>([])
+
+    useEffect(() => {
+        async function getPets() {
+        const response = await fetch('http://127.0.0.1:8000/api/pets/');
+        const data = await response.json();
+        setPets(data);
+        console.log('🍪🍪', data)
+    };
+        getPets();
+    }, []);
+    
+    
+
+      return (
+    <>
+      <p>coucou</p>
+      
+        {pets.map((pet) => (
+          <p key={pet.id}>
+            {pet.pet_name} {pet.breed}
+          </p>
+        ))}
+     
+    </>
+  );
 }
