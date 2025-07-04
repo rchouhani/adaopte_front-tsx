72afa34341992ca5c9bb491a32b261f613187af3
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-07-04 15:11:15 +0200

    update: component cards

--- src/app/components/QuestionsArea.tsx
+++ src/app/components/QuestionsArea.tsx
@@ -1,26 +1,29 @@
-import QuestionsCard from "./QuestionsCard";
+import QuestionsCard, { QuestionsCardsProps } from "./QuestionsCard";
 
-import { cardsContent } from "../volunteersView/beVolunteer/page";
-
-export default function QuestionsArea() {
+type QuestionsAreaProps = {
+  title: string;
+  subtitle: string;
+  cards: QuestionsCardsProps[];
+};
+export default function QuestionsArea({
+  title,
+  subtitle,
+  cards,
+}: QuestionsAreaProps) {
   return (
     <main className="flex flex-col items-center text-center p-[30px] bg-(--light-color) -mt-[70px] pt-[70px]">
-      <h2>Comment ça marche ?</h2>
-      <h3>
-        Adopter un animal est un engagement sérieux et à long terme. Voici votre
-        processus en quelques étapes simples.
-      </h3>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
 
       <section className="flex flex-row gap-4">
-        {cardsContent.map((card, index) => 
-        <QuestionsCard 
-          key={index}
-          title={card.title}
-          subtitle={card.subtitle}
-          paragraph={card.paragraph}
-        />
-        )}
-        
+        {cards.map((card, index) => (
+          <QuestionsCard
+            key={index}
+            title={card.title}
+            subtitle={card.subtitle}
+            paragraph={card.paragraph}
+          />
+        ))}
       </section>
     </main>
   );
--- src/app/components/QuestionsCard.tsx
+++ src/app/components/QuestionsCard.tsx
@@ -1,4 +1,4 @@
-type QuestionsProps = {
+export type QuestionsCardsProps = {
   title: string;
   subtitle: string;
   paragraph: string;
@@ -8,11 +8,13 @@ export default function QuestionsCard({
   title,
   subtitle,
   paragraph,
-}: QuestionsProps) {
+}: QuestionsCardsProps) {
   return (
-    <article className="w-2/4 h-70 border border-white shadow-[0_5px_15px_rgba(0,0,0,0.1)] rounded-[20px]">
+    <article className="w-2/4 h-auto border border-white shadow-[0_5px_15px_rgba(0,0,0,0.1)] rounded-[20px]">
       <h4 className="p-[10px] text-[2em] text-center">{title}</h4>
-      <p className="text-(--dark-color) text-center font-extrabold p-[10px] w-[90%]">{subtitle}</p>
+      <p className="text-(--dark-color) text-center font-extrabold p-[10px] w-[90%]">
+        {subtitle}
+      </p>
       <p className="text-(--dark-color) p-[10px] text-center">{paragraph}</p>
     </article>
   );
