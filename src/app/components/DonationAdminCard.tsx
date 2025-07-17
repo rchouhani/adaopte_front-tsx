300ae1b48dcb964b6f23011fb6f2d5ccf377e08a
Edwige08 <edwige.saves@gmail.com>
2025-07-17 12:55:18 +0200

    add: display donations in admin view

--- src/app/components/DonationAdminCard.tsx
+++ src/app/components/DonationAdminCard.tsx
@@ -1,7 +1,33 @@
-export default function DonationAdminCard(props: {donator: string, amountDonated: number}) {
+type Availability = {
+    frequency: string,
+    id: number
+}
+
+type Users = {
+    id: number,
+    availability: Availability
+    birthdate: string,
+    firstname: string,
+    lastname: string,
+    email: string,
+    phone: string,
+    city: string,
+    zipcode: string,
+    motivation: string,
+    volunteer: string,
+    adoptant: string,
+}
+
+type DonationAdminCardProps = {
+    id: number,
+    donator?: Users,
+    amountDonated: number,
+}
+
+export default function DonationAdminCard({ id, amountDonated }: DonationAdminCardProps) {
     return (
         <div className="flex flex-row gap-2 p-2 border border-1 rounded-sm m-2">
-            <p>{props.donator} - {props.amountDonated} €</p>
+            <p>Don n°{id}: {amountDonated} €</p>
         </div>
     )
 }
